1965da381e89bde3cab0564050805b5752511bec
Connie <conniefeng28@gmail.com>
2014-10-21 16:40:43 -0500

    Added comments to Subset

--- hw02/src/Subset.java
+++ hw02/src/Subset.java
@@ -1,14 +1,25 @@
-
+/**
+ * A class that takes an integer k, reads in a sequence of strings, and prints
+ * out exactly k of them, uniformly at random. Each item from the sequence can
+ * be printed out at most once.
+ */
 public class Subset {
-	   public static void main(String[] args){
-		   int in = Integer.parseInt(args[0]);
-		   RandomizedQueue<String> rq = new RandomizedQueue<String>();
-		   while (!StdIn.isEmpty()){
-			   rq.enqueue(StdIn.readString());
-		   }
-		   for (int i =0; i< in; i++){
-			   StdOut.println(rq.dequeue());
-		   }
-		   
-	   }
+	/**
+	 * Run.
+	 * 
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		int in = Integer.parseInt(args[0]);
+		RandomizedQueue<String> rq = new RandomizedQueue<String>();
+		
+		while (!StdIn.isEmpty()) {
+			rq.enqueue(StdIn.readString());
+		}
+		
+		for (int i = 0; i < in; i++) {
+			StdOut.println(rq.dequeue());
+		}
+
+	}
 }
